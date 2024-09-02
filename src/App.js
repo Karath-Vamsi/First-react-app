1adac2298740b13657219c180713582ba981c23a
Karath-Vamsi <ugs22047_aids.vamsi@cbit.org.in>
2024-09-03 00:54:59 +0530

    app created

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+const API_URL = "https://www.omdbapi.com?apikey=b3136576";
+
+
+const App = () => {
+
+    const [movies, setMovies] = useState([])
+    const [search, setSearch] = useState('')
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+      };
+
+    useEffect(() => {
+        searchMovies("");
+    },[])
+
+    return (
+        <div className='app'>
+            <h1>Movie Database</h1>
+
+            <div className='search'>
+                <input 
+                    placeholder='Search for a movie...'
+                    value={search}
+                    onChange={(e) => {setSearch(e.target.value)}}
+                />
+                <img 
+                    src={SearchIcon} 
+                    alt='Search Icon'
+                    onClick={() => {searchMovies(search)}}
+                />
+            </div>
+
+            {
+                movies?.length > 0
+                ? (
+                    <div className='container'>
+                        {
+                            movies.map((movie) => {
+                                return <MovieCard movie={movie} />
+})
+                        } 
+                    </div>
+                ) : (
+                    <h3 id='opener'>Search some movies...!</h3>
+                )
+            }
+
+        </div>
+    );
+}
+
+export default App;
